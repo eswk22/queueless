dde0faa9b9ce50091ac5e5c5896ff9fd5ebec811
Eswaran Krishnamoorthy <eswaran.krishnamoorthy@gmail.com>
2020-03-30 15:33:09 -0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'src/welcomePage.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.purple,
+        textTheme:GoogleFonts.latoTextTheme(textTheme).copyWith(
+           body1: GoogleFonts.montserrat(textStyle: textTheme.body1),
+         ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+    );
+  }
+}
